91a8870ec1558630b1fd843a766a375b66f1f494
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-04-07 15:49:53 +0530

    Time: 12 ms (54.83%), Space: 68.5 MB (65.51%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,14 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] arr) {
-        HashMap<Integer,Integer>fmap=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            fmap.put(arr[i],fmap.getOrDefault(arr[i],0)+1);
-        }
+        HashSet<Integer>set=new HashSet<>();
         
-        for(Integer key:fmap.keySet()){
-            if(fmap.get(key)>1){
-                return true;
-            }
+        for(int ele:arr){
+            if(set.contains(ele))return true;
+            else set.add(ele);
         }
         return false;
     }
