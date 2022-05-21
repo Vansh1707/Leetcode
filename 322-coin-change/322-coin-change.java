2b910f503915de91d57950724334f0dc77b004e6
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-05-21 10:54:06 +0530

    Time: 24 ms (59.09%), Space: 45.2 MB (42.44%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int coinChange(int[] arr, int Tar) {
+        int[] dp = new int[Tar + 1];
+        Arrays.fill(dp, (int) 1e9);
+        dp[0] = 0;
+        for (int tar = 1; tar <= Tar; tar++) {
+            for (int ele : arr) {
+                if (tar - ele >= 0)
+                    dp[tar] = Math.min(dp[tar - ele] + 1, dp[tar]);
+            }
+        }
+        return dp[Tar] != (int) 1e9 ? dp[Tar] : -1;
+    }
+}
