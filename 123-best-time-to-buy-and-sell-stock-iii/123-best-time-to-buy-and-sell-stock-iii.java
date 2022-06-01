1ca8ab7bd2572e021116a4378aa13da37fd63b53
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-06-01 14:02:18 +0530

    Time: 3 ms (89.14%), Space: 78.5 MB (54.95%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,36 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
+        int dpi20=0;
+        int dpi21=-(int)1e9;
+        int dpi10=0;
+        int dpi11=-(int)1e9;
         
-        int[][][]dp=new int[n][3][2];
-        
-        for(int i=0;i<n;i++){
-            for(int k=0;k<3;k++){
-                for(int x=0;x<2;x++){
-                    if(k==0){
-                        if(x==0){
-                            dp[i][k][x]=0;
-                        }else{
-                            dp[i][k][x]=-(int)1e9;
-                        }
-                    }else{
-                        if(i==0){
-                            if(x==0){
-                                dp[i][k][x]=Math.max(0,-(int)1e9+prices[i]);
-                            }else{
-                                dp[i][k][x]=Math.max(-(int)1e9,0-prices[i]);
-                            }
-                        }else{
-                            if(x==0){
-                                dp[i][k][x]=Math.max(dp[i-1][k][0],dp[i-1][k][1]+prices[i]);
-                            }else{
-                                dp[i][k][x]=Math.max(dp[i-1][k][1],dp[i-1][k-1][0]-prices[i]);
-                            }
-                        }
-                    }
-                }
-            }
+        for(int i=0;i<prices.length;i++){
+            dpi20=Math.max(dpi20,dpi21+prices[i]);
+            dpi21=Math.max(dpi21,dpi10-prices[i]);
+            dpi10=Math.max(dpi10,dpi11+prices[i]);
+            dpi11=Math.max(dpi11,0-prices[i]);
         }
-        return dp[n-1][2][0];
+        return Math.max(dpi10,dpi20);
     }
 }
