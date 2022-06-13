bab15ccf9aec0df64909fac70ab491509324319f
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-06-13 11:32:04 +0530

    Time: 10 ms (16.62%), Space: 42.8 MB (75.14%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -3,21 +3,14 @@ class Solution {
         if(triangle.size()==1)return triangle.get(0).get(0);
         int n=triangle.size();
         
-        for(int i=1;i<n;i++){
+        for(int i=n-2;i>=0;i--){
             int m=triangle.get(i).size();
             for(int j=0;j<m;j++){
-                int sum=0;
-                if(j==0){
-                    sum=triangle.get(i).get(j)+triangle.get(i-1).get(j);
-                }else if(j==m-1){
-                    sum=triangle.get(i).get(j)+triangle.get(i-1).get(triangle.get(i-1).size()-1);
-                }else{
-                    int min=Math.min(triangle.get(i-1).get(j),triangle.get(i-1).get(j-1));
-                    sum=min + triangle.get(i).get(j);
-                }
+                int min=Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1));
+                int sum=min+triangle.get(i).get(j);
                 triangle.get(i).set(j,sum);
-            }
-        }
-        return Collections.min(triangle.get(triangle.size()-1));
+            }                
+        }            
+        return triangle.get(0).get(0) ;
     }
 }
