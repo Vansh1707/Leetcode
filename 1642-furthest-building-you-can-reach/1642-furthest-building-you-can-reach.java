9ba8f73ccf96aaef6defb3929118129bb0ce0796
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-06-21 14:29:53 +0530

    Time: 28 ms (60.66%), Space: 79.4 MB (34.60%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        int n=heights.length;
+        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->{
+            return a-b;
+        });
+        for(int i=0;i<n-1;i++){
+            int heidiff=heights[i+1]-heights[i];
+            if(heidiff>0)
+                pq.add(heidiff);
+            
+            if(pq.size()>ladders){
+                bricks=bricks-pq.remove();
+            }
+            if(bricks<0)
+                return i;
+        }
+        return n-1;
+    }
+}
