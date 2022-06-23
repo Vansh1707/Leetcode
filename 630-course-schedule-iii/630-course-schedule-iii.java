8705ccec957e6d2ce50c0eb197245a08eb2ab91d
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-06-23 11:19:40 +0530

    Time: 75 ms (23.33%), Space: 75.4 MB (10.00%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,17 +1,25 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->b-a);
-        
-        Arrays.sort(courses,(a,b)->a[1]-b[1]);
-        
+        Arrays.sort(courses, (a,b)-> a[1]==b[1] ? a[0]-b[0] : a[1]-b[1]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
         int time = 0;
-        for(int[] course: courses) {
-            int duration = course[0], lastDay = course[1];
-            time += duration;
-            pq.add(duration);
-            // if pass the last day, remove courses these take longest time.
-            if(time > lastDay) {
-                time -= pq.remove();
+        
+        for(int[] course : courses) {
+            //Check if we consider current course : if duration <= lastday
+            if(course[0] <= course[1]) {
+                //current course can be completed with the lastday given
+                if(course[0]+time <= course[1]) {
+                    pq.add(course[0]);
+                    time+=course[0];
+                }
+                else {
+                    //Check if we can swap
+                    if(pq.peek() > course[0]) {
+                        time-=pq.remove();
+                        time+=course[0];
+                        pq.add(course[0]);
+                    }
+                }
             }
         }
         return pq.size();
