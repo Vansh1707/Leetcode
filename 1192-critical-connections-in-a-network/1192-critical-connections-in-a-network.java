0db414ef8d2a79445f73393dc24a019a750fd334
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-06-28 18:26:03 +0530

    Time: 180 ms (57.72%), Space: 340.7 MB (34.93%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,63 +1,52 @@
 class Solution {
-    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        // Prepare the adjaceny list from the given edges 
-        ArrayList<ArrayList<Integer>> adjList = createAdjacenyList(n, connections);
+    int[]low,disc,par;
+    int time=0;
+    boolean vis[];
+    
+    public void dfs(ArrayList<ArrayList<Integer>>graph,int src,List<List<Integer>>ans){
         
-        // Stores critical edges/answer
-        List<List<Integer>> ans = new ArrayList();
+        disc[src]=low[src]=time++;
+        vis[src]=true;
+        for(int i=0;i<graph.get(src).size();i++){
+            int nbr=graph.get(src).get(i);
+            
+            if(par[src]==nbr)continue;
+            else if(vis[nbr]){
+                low[src]=Math.min(low[src],disc[nbr]);
+            }else{
+                par[nbr]=src;
+                dfs(graph,nbr,ans);
+                
+                low[src]=Math.min(low[src],low[nbr]);
+                if(disc[src]<low[nbr]){
+                    List<Integer>smallans=new ArrayList<>();
+                    smallans.add(src);
+                    smallans.add(nbr);
+                    ans.add(smallans);
+                }
+            }
+        }
+    }
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> Edges) {
+        ArrayList<ArrayList<Integer>>graph=new ArrayList<>();
+        for(int i=0;i<n;i++)graph.add(new ArrayList<>());
         
-        // Stores discovery time and minimum discovery time of each node in the graph
-        int[] dis = new int[n];
-        int[] minDis = new int[n];
+        for(int i=0;i<Edges.size();i++){
+            int u=Edges.get(i).get(0);
+            int v=Edges.get(i).get(1);
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
         
-        // Running timer, starts from 1.
-        int[] timer = new int[] { 1 };
+        disc=new int[n];
+        low=new int[n];
+        par=new int[n];
+        vis=new boolean[n];
         
-        // DFS from 0th node, since there is no parent, set parent to -1.
-        dfs(0, -1, adjList, dis, minDis, timer, ans);
+        List<List<Integer>>ans=new ArrayList<>();
+        
+        dfs(graph,0,ans);
         
         return ans;
-    }
-    
-    private ArrayList<ArrayList<Integer>> createAdjacenyList(int n, List<List<Integer>> edges) {
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList();
-        
-        for(int i=0; i<n; i++)
-            adjList.add( new ArrayList() );
-        
-        for(List<Integer> edge : edges) {
-            int u = edge.get(0);
-            int v = edge.get(1);
-            adjList.get( u ).add( v );
-            adjList.get( v ).add( u );
-        }
-        
-        return adjList;
-    }
-    
-    private void dfs(int node, int parent, ArrayList<ArrayList<Integer>> adjList, int[] dis, int[] minDis, 
-                     int[] timer, List<List<Integer>> ans) {
-
-        // Set minimum discovery time and discovery time to current time
-        minDis[node] = dis[node] = timer[0]++;
-        
-        for(int adj : adjList.get(node)) {
-            // Skip if the adjacent node is the parent
-            if( adj == parent ) 
-                continue;
-            
-            // If adjacent node is not yet visited, DFS on the adjacent node
-            if( dis[adj] == 0 ) 
-                dfs(adj, node, adjList, dis, minDis, timer, ans);
-            
-            /* If adjacent node can be discovered early than the current node, 
-            than update the minimum discovery time of current node as well */
-            minDis[node] = Math.min(minDis[node], minDis[adj]);
-        }
-        
-        /* If current node can only be disovered from the parent node, than the
-        parent to current node's edge is critical edge (bridge) */
-        if( parent != -1 && minDis[node] > dis[parent] ) 
-            ans.add( new ArrayList() {{ add(parent); add(node); }});
     }
 }
