da30d54567f3d8a188a13782b0e4f002373d4af1
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-07-06 10:41:15 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (57.51%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,12 +1,13 @@
 class Solution {
+    public int fibomem(int n,int[]dp){
+        if(n<=1)return dp[n]=n;
+        if(dp[n]!=0)return dp[n];
+        int ans=fibomem(n-1,dp)+fibomem(n-2,dp);
+        return dp[n]=ans;
+    }
     public int fib(int n) {
-        int a=0,b=1;
+        int[]dp=new int[n+1];
+        return fibomem(n,dp);
         
-        while(n-->0){
-            int sum=a+b;
-            a=b;
-            b=sum;
-        }
-        return a;
     }
 }
