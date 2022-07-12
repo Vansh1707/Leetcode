3c41153a3cf6db2e0c89832a8407f519c8c91c65
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-07-12 12:11:49 +0530

    Time: 357 ms (12.70%), Space: 41.8 MB (56.74%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -2,7 +2,7 @@ class Solution {
     // like partition in k eqaul subset 
     
      public boolean canPartitionSubsets(int[] arr,int idx, int k,int sumSF,int tar,boolean vis[]) {
-        if(k==0)return true;
+        if(k==1)return true;
         if(sumSF>tar) return false;
         if(tar==sumSF){
             return canPartitionSubsets(arr,0,k-1,0,tar,vis);
