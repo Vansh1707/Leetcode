01c5cf470af25fba0ce56fea16230bf127b95ebc
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-07-18 21:58:08 +0530

    Time: 1 ms (99.56%), Space: 48.6 MB (50.61%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,17 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-        LinkedList<Integer>st=new LinkedList<>();
-        int curr=0,water=0;
+        int i=0,j=height.length-1,water=0,maxl=0,maxr=0;
         
-        while(curr<height.length){
-            while(st.size()>0 &&height[curr]>height[st.getFirst()]){
-                int top=st.removeFirst();
-                if(st.size()==0)break;
-                int diff=curr-st.getFirst()-1;
-                int fill=diff*(Math.min(height[curr],height[st.getFirst()])-height[top]);
-                water+=fill;
+        while(i<j){
+            if(height[i]<=height[j]){
+                //maxleft
+                maxl=Math.max(maxl,height[i]);
+                water+=maxl-height[i];
+                i++;
+            }else{
+                //max right
+                maxr=Math.max(maxr,height[j]);
+                water+=maxr-height[j];
+                j--;
             }
-            st.addFirst(curr++);
         }
         return water;
     }
