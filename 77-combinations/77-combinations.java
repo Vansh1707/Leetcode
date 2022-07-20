4c8ec980e2dea5ad1d9aef232590f70e8d8737ea
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-07-20 23:20:47 +0530

    Time: 45 ms (33.47%), Space: 54.6 MB (70.22%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int combine(int n, int k,int idx,List<List<Integer>>ans,List<Integer>smallans) {
+        if(k==0){
+            List<Integer>base=new ArrayList<>(smallans);
+            ans.add(base);
+            return 1;
+        }
+        int count=0;
+        for(int i=idx;i<=n;i++){            
+            smallans.add(i);
+            count+=combine(n,k-1,i+1,ans,smallans);
+            smallans.remove(smallans.size()-1);   
+        }
+        return count;
+        
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>smallans=new ArrayList<>();
+        combine(n,k,1,ans,smallans);
+        return ans;
+    }
+}
