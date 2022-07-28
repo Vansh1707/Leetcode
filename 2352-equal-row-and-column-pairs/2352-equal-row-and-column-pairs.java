58fff1ab939777c6f76d5693da4158df73ebe325
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-07-28 21:46:44 +0530

    Time: 46 ms (80.00%), Space: 50.4 MB (100.00%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,25 +1,24 @@
 class Solution {
-    public int equalPairs(int[][] grid) {
-        HashMap<String, Integer> h = new HashMap();
-        int ans = 0;
-        
-        for(int i=0; i<grid.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for(int j=0; j<grid[i].length; j++) {
-                sb.append(grid[i][j]).append("_");
-            }
-            h.put(sb.toString(),h.getOrDefault(sb.toString(), 0)+1);
-        }
-        
-        
-        for(int i=0; i<grid[0].length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for(int j=0; j<grid.length; j++) {
-                sb.append(grid[j][i]).append("_");
-            }
-            if (h.containsKey(sb.toString())) ans+=h.get(sb.toString());
-        }
-        
-        return ans;
-    }
+	public int equalPairs(int[][] grid) {
+		int ans=0;
+		for(int i=0;i<grid.length;i++)
+		{
+			for(int j=0;j<grid.length;j++)
+			{
+				if(grid[i][0]==grid[0][j])
+				{
+					if(check(grid,i,j)) ans++;
+				}
+			}
+		}
+		return ans;
+	}
+	boolean check(int[][] grid, int i, int j){
+		for(int k=0;k<grid.length;k++)
+		{
+			if(grid[i][k]!=grid[k][j])
+				return false;
+		}
+		return true;
+	}
 }
