639ecac9172dfa78797d76423474b234048615db
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-10-01 13:10:06 +0530

    Time: 2 ms (70.22%), Space: 42.5 MB (37.39%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numDecodings_tab(String s,int IDX,int[]dp){
+        for(int idx=s.length();idx>=0;idx--){
+            if(idx==s.length()){
+               dp[idx]=1;
+                continue;
+            }
+            // if(dp[idx]!=-1) return dp[idx];
+
+            char ch=s.charAt(idx);
+            if(ch=='0'){
+                dp[idx]=0;
+                continue;
+            }
+            int count=dp[idx+1];
+
+            if(idx<s.length()-1){
+                char ch1=s.charAt(idx+1);
+                int num= (ch-'0')*10 + (ch1-'0');
+                if(num<=26)
+                    count+=dp[idx+2];
+            }
+            dp[idx]=count;
+        }
+        return dp[IDX];
+    }
+    public int numDecodings(String s) {
+        int n=s.length();
+        int[]dp=new int[n+1];
+        
+        Arrays.fill(dp,-1);
+        
+        return numDecodings_tab(s,0,dp);
+    }
+}
