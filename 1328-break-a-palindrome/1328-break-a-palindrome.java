24b79dab26fb5f041f3d7a1fbdcc66fa0cc6900a
Vansh1707 <87300064+Vansh1707@users.noreply.github.com>
2022-10-10 13:26:59 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (92.59%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+   public String breakPalindrome(String palindrome) {
+        int n = palindrome.length();
+        if (n == 1){
+            return "";
+        }
+        boolean o = false;
+        char[] c = palindrome.toCharArray();
+        for (int i = 0; i < n/2; i++){
+            if (c[i] != 'a'){
+                c[i] = 'a';
+                return String.valueOf(c);
+            }
+        }
+        c[n-1] = 'b';
+        return String.valueOf(c);
+    }
+}
